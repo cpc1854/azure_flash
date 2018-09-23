c508455c1dffbc215536627d10b341cf4f832be3
cpc1854 <cpc1854@gmail.com>
2018-09-23 08:38:27 +0800

    a

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
-
+def index():
+    return render_template(
+        'indexh.html',**locals())
+##def hello_world():
+##  return 'Hey its Python Flask application on Azure!'
 if __name__ == '__main__':
   app.run()
-   
